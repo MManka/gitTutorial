9d08f556b77468de9e816a2c6a9d2213085d8bb2
Marlin Manka <marlinmanka1@gmail.com>
2017-02-15 22:46:45 -0500

    Added Course class and made changes to Student and Instructor.

---
+++ src/Course.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Marlin on 2/15/2017.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course (String cname, String description)
+    {
+        this.cname = cname;
+        this.description = description;
+    }
+}
--- src/Student.java
+++ src/Student.java
@@ -3,10 +3,16 @@
  */
 public class Student extends Person {
     String major;
+    List<Course> courseList;
 
     public Student(String name, String emailAddress, String major) {
         super(name, emailAddress);
         this.major = major;
+    }
+
+    public void addCourse(Course c)
+    {
+
     }
 
     public String toString() {
